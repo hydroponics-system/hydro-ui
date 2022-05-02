70ce8f42c9fbec99a697c6b403453d3d7f56c6cd
Sam Butler <67570772+sambutler1017@users.noreply.github.com>
2022-05-02 19:14:19 -0400

    HYDRO-UI-1: Adding login screen (#1)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ToastrModule } from 'ngx-toastr';
 import { InsiteKitModule } from 'projects/insite-kit/src/insite-kit.module';
 import { BasicAuthHtppInterceptorService } from 'projects/insite-kit/src/service/http-interceptor/basic-auth-htpp-interceptor.service';
@@ -18,6 +19,7 @@ import { AppRoutingModule } from '../app-routing.module';
     InsiteKitModule,
     BrowserAnimationsModule,
     FormsModule,
+    FontAwesomeModule,
     ReactiveFormsModule,
     ToastrModule.forRoot({
       positionClass: 'toast-top-right',
@@ -33,6 +35,7 @@ import { AppRoutingModule } from '../app-routing.module';
     FormsModule,
     ReactiveFormsModule,
     ToastrModule,
+    FontAwesomeModule,
   ],
   providers: [
     {
